3359cd078618c6c8bf48568f7b12bd3e4caaa61a
miacro <fqguozhou@gmail.com>
2019-03-27 18:10:18 +0800

    update model

--- recommendedsystem/model.py
+++ recommendedsystem/model.py
@@ -3,18 +3,13 @@ from recommendedsystem.netflix_prize_dataset import Dataset
 import os
 import tensorflow as tf
 import logging
+
 # tf.enable_eager_execution()
 
-if __name__ == "__main__":
+
+def create_model():
     MOVIE_COUNT = 17771
     CONSUMER_COUNT = 2649430
-    DATASET_DIR = os.path.expanduser("~/datasets/netflix-prize/tfrecord")
-    # CHECKPOINT_PATH = "./checkpoint/model-{epoch:08d}.ckpt"
-    CHECKPOINT_PATH = "./checkpoint/model.h5"
-    MODEL_PATH = "./model/model.h5"
-
-    EPOCHS = 3
-    BATCH_SIZE = 60
 
     input_movie = keras.layers.Input(name="movieindex", shape=(1, ))
     input_consumer = keras.layers.Input(name="consumerindex", shape=(1, ))
@@ -32,11 +27,63 @@ if __name__ == "__main__":
     x = keras.layers.Activation(activation="sigmoid")(x)
     y = keras.layers.Dense(1)(x)
     model = keras.models.Model([input_movie, input_consumer], y)
-    model.compile(loss="mean_squared_error", optimizer='adadelta')
+    model.compile(
+        loss="mean_squared_error",
+        optimizer='adadelta',
+        metrics=['accuracy'],
+    )
+    return model
+
+
+def load_model(model, checkpoint):
+    latest_checkpoint = None
+    if os.path.exists(checkpoint):
+        latest_checkpoint = checkpoint
+    else:
+        latest_checkpoint = tf.train.latest_checkpoint(
+            os.path.dirname(checkpoint))
+    if latest_checkpoint:
+        logging.info("load model from {}".format(latest_checkpoint))
+        model.load_weights(latest_checkpoint)
+
+
+def train_model(model, dataset, epochs, checkpoint):
+    checkpoint_callback = keras.callbacks.ModelCheckpoint(
+        checkpoint,
+        save_weights_only=True,
+        verbose=1,
+    )
+    model.fit(
+        dataset,
+        epochs=epochs,
+        steps_per_epoch=24,
+        callbacks=[checkpoint_callback],
+    )
+    # model.save_weights(MODEL_PATH)
+
+
+def evaluate_model(model, dataset):
+    loss, accuracy = model.evaluate(dataset, steps=1)
+    return loss, accuracy
+
+
+def main():
+    DATASET_DIR = os.path.expanduser("~/datasets/netflix-prize/tfrecord")
+    # CHECKPOINT_PATH = "./checkpoint/model-{epoch:08d}.ckpt"
+    CHECKPOINT_PATH = "./checkpoint/model.h5"
+    EPOCHS = 1000
+    BATCH_SIZE = 60
+
+    model = create_model()
+    model.summary()
+    load_model(model, CHECKPOINT_PATH)
 
     dataset = Dataset(directory=DATASET_DIR)
-    dataset = dataset.tfdataset("trainingset")
-    dataset = dataset.batch(BATCH_SIZE)
+    dataset_train = dataset.tfdataset("trainingset")
+    dataset_train = dataset_train.batch(BATCH_SIZE)
+
+    dataset_evaluate = dataset.tfdataset("qualifyingset")
+    dataset_evaluate = dataset_evaluate.batch(1)
 
     def convert(x):
         return {
@@ -44,27 +91,13 @@ if __name__ == "__main__":
             "consumerindex": x["consumerindex"]
         }, x["rate"]
 
-    dataset = dataset.map(convert)
+    dataset_train = dataset_train.map(convert)
+    dataset_evaluate = dataset_evaluate.map(convert)
 
-    model.summary()
-    checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        CHECKPOINT_PATH,
-        save_weights_only=True,
-        verbose=1,
-    )
-    latest_checkpoint = None
-    if os.path.exists(CHECKPOINT_PATH):
-        latest_checkpoint = CHECKPOINT_PATH
-    else:
-        latest_checkpoint = tf.train.latest_checkpoint(
-            os.path.dirname(CHECKPOINT_PATH))
-    if latest_checkpoint:
-        logging.info("load model from {}".format(latest_checkpoint))
-        model.load_weights(latest_checkpoint)
-    model.fit(
-        dataset,
-        epochs=EPOCHS,
-        steps_per_epoch=24,
-        callbacks=[checkpoint_callback],
-    )
-    # model.save_weights(MODEL_PATH)
+    train_model(model, dataset_train, EPOCHS, CHECKPOINT_PATH)
+    loss, accuracy = evaluate_model(model, dataset_evaluate)
+    print("accuracy: {:5.2f}%".format(accuracy * 100))
+
+
+if __name__ == "__main__":
+    main()
